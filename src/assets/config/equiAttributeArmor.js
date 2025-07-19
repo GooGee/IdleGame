1fb4fb63dd2ea07459dadbc30047108c223f7846
GooGee <mou-gj@live.com>
2025-07-19 09:11:20 +0800

    fix

--- src/assets/config/equiAttributeArmor.js
+++ src/assets/config/equiAttributeArmor.js
@@ -34,179 +34,179 @@ export const equiAttributeArmor = {
         color: '#ff0000', extraEntryNum: 5,
       }],
       extraEntryArmor: [{
-        'value': '11',
-        'showVal': '+11',
+        value: '11',
+        showVal: '+11',
         type: 'ATK',
-        'name': '攻击力'
+        name: '攻击力'
       }, {
         type: 'HP',
-        'value': '20',
-        'showVal': '+20',
-        'name': '生命值'
+        value: '20',
+        showVal: '+20',
+        name: '生命值'
       }, {
         type: 'DEF',
-        'value': '8',
-        'showVal': '+8%',
-        'name': '防御力'
+        value: '8',
+        showVal: '+8%',
+        name: '防御力'
       }, {
-        'value': '11%',
-        'showVal': '+11%',
+        value: '11%',
+        showVal: '+11%',
         type: 'DEFPERCENT',
-        'name': '防御力'
+        name: '防御力'
       }, {
-        'value': '11%',
-        'showVal': '+11%',
+        value: '11%',
+        showVal: '+11%',
         type: 'HPPERCENT',
-        'name': '生命值'
+        name: '生命值'
       },],
       uniqueCategoryArmor: [{
         name: '争执连身衣',
         des: '',
         iconSrc: './icons/U_Armor01.png',
         entry: [{
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'DEF',
-          'name': '防御力'
+          name: '防御力'
         }, {
           type: 'HP',
-          'valCoefficient': 2.6,
-          'value': '8',
-          'showVal': '8',
-          'name': '生命值'
+          valCoefficient: 2.6,
+          value: '8',
+          showVal: '8',
+          name: '生命值'
         }, {
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }]
       }, {
         name: '剑豪盔甲',
         des: '',
         iconSrc: './icons/U_Armor02.png',
         entry: [{
-          'valCoefficient': 2.6,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 2.6,
+          value: '11',
+          showVal: '+11',
           type: 'DEF',
-          'name': '防御力'
+          name: '防御力'
         }, {
           type: 'HP',
-          'valCoefficient': 1.2,
-          'value': '8',
-          'showVal': '8',
-          'name': '生命值'
+          valCoefficient: 1.2,
+          value: '8',
+          showVal: '8',
+          name: '生命值'
         }, {
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }]
       }, {
         name: '隐武士铠甲',
         des: '',
         iconSrc: './icons/U_Armor03.png',
         entry: [{
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'DEF',
-          'name': '防御力'
+          name: '防御力'
         }, {
           type: 'HP',
-          'valCoefficient': 1.2,
-          'value': '8',
-          'showVal': '8',
-          'name': '生命值'
+          valCoefficient: 1.2,
+          value: '8',
+          showVal: '8',
+          name: '生命值'
         }, {
-          'valCoefficient': 2.6,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 2.6,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }]
       }, {
         name: '芬撒里尔追踪者',
         des: '',
         iconSrc: './icons/U_Armor04.png',
         entry: [{
-          'valCoefficient': 2.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 2.2,
+          value: '11',
+          showVal: '+11',
           type: 'DEF',
-          'name': '防御力'
+          name: '防御力'
         }, {
           type: 'HP',
-          'valCoefficient': 1.2,
-          'value': '8',
-          'showVal': '8',
-          'name': '生命值'
+          valCoefficient: 1.2,
+          value: '8',
+          showVal: '8',
+          name: '生命值'
         }, {
-          'valCoefficient': 1.6,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.6,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }]
       }, {
         name: '肃清者戎衣',
         des: '',
         iconSrc: './icons/U_Armor05.png',
         entry: [{
-          'valCoefficient': 2.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 2.2,
+          value: '11',
+          showVal: '+11',
           type: 'DEF',
-          'name': '防御力'
+          name: '防御力'
         }, {
           type: 'HP',
-          'valCoefficient': 1.6,
-          'value': '8',
-          'showVal': '8',
-          'name': '生命值'
+          valCoefficient: 1.6,
+          value: '8',
+          showVal: '8',
+          name: '生命值'
         }, {
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         },]
       }, {
         name: '红月羽衣',
         des: '',
         iconSrc: './icons/U_Armor06.png',
         entry: [{
-          'valCoefficient': 2.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 2.2,
+          value: '11',
+          showVal: '+11',
           type: 'DEF',
-          'name': '防御力'
+          name: '防御力'
         }, {
           type: 'HP',
-          'valCoefficient': 2.8,
-          'value': '8',
-          'showVal': '8',
-          'name': '生命值'
+          valCoefficient: 2.8,
+          value: '8',
+          showVal: '8',
+          name: '生命值'
         }]
       }, {
         name: '狂龙战士盔甲',
         des: '',
         iconSrc: './icons/U_Armor07.png',
         entry: [{
-          'valCoefficient': 3.8,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 3.8,
+          value: '11',
+          showVal: '+11',
           type: 'DEF',
-          'name': '防御力'
+          name: '防御力'
         }, {
           type: 'HP',
-          'valCoefficient': 1.2,
-          'value': '8',
-          'showVal': '8',
-          'name': '生命值'
+          valCoefficient: 1.2,
+          value: '8',
+          showVal: '8',
+          name: '生命值'
         },]
       },],
       categoryArmor: [
@@ -215,17 +215,17 @@ export const equiAttributeArmor = {
           des: '够肉才能输出',
           iconSrc: './icons/A_A2.png',
           entry: [{
-            'valCoefficient': 2,
-            'value': '11',
-            'showVal': '+11',
+            valCoefficient: 2,
+            value: '11',
+            showVal: '+11',
             type: 'DEF',
-            'name': '防御力'
+            name: '防御力'
           }, {
             type: 'HP',
-            'valCoefficient': 0.6,
-            'value': '8',
-            'showVal': '8',
-            'name': '生命值'
+            valCoefficient: 0.6,
+            value: '8',
+            showVal: '8',
+            name: '生命值'
           }]
         },
         {
@@ -233,17 +233,17 @@ export const equiAttributeArmor = {
           des: '六级战士使用的重型铠甲',
           iconSrc: './icons/A_A5.png',
           entry: [{
-            'valCoefficient': 1.1,
-            'value': '11',
-            'showVal': '+11',
+            valCoefficient: 1.1,
+            value: '11',
+            showVal: '+11',
             type: 'DEF',
-            'name': '防御力'
+            name: '防御力'
           }, {
             type: 'HP',
-            'valCoefficient': 0.8,
-            'value': '8',
-            'showVal': '8',
-            'name': '生命值'
+            valCoefficient: 0.8,
+            value: '8',
+            showVal: '8',
+            name: '生命值'
           }]
         },
         {
@@ -251,23 +251,23 @@ export const equiAttributeArmor = {
           des: '舍弃了防御性能的轻甲，因为更加轻便，攻击性能更加突出',
           iconSrc: './icons/A_A7.png',
           entry: [{
-            'valCoefficient': 0.7,
-            'value': '11',
-            'showVal': '+11',
+            valCoefficient: 0.7,
+            value: '11',
+            showVal: '+11',
             type: 'DEF',
-            'name': '防御力'
+            name: '防御力'
           }, {
             type: 'HP',
-            'valCoefficient': 0.5,
-            'value': '8',
-            'showVal': '8',
-            'name': '生命值'
+            valCoefficient: 0.5,
+            value: '8',
+            showVal: '8',
+            name: '生命值'
           }, {
-            'valCoefficient': 0.5,
-            'value': '11',
-            'showVal': '+11',
+            valCoefficient: 0.5,
+            value: '11',
+            showVal: '+11',
             type: 'ATK',
-            'name': '攻击力'
+            name: '攻击力'
           }]
         },
         {
@@ -275,17 +275,17 @@ export const equiAttributeArmor = {
           des: '似乎会给使用者提供生命气息',
           iconSrc: './icons/A_A3.png',
           entry: [{
-            'valCoefficient': 0.9,
-            'value': '11',
-            'showVal': '+11',
+            valCoefficient: 0.9,
+            value: '11',
+            showVal: '+11',
             type: 'DEF',
-            'name': '防御力'
+            name: '防御力'
           }, {
             type: 'HP',
-            'valCoefficient': 1.2,
-            'value': '8',
-            'showVal': '8',
-            'name': '生命值'
+            valCoefficient: 1.2,
+            value: '8',
+            showVal: '8',
+            name: '生命值'
           }]
         },
         {
@@ -293,23 +293,23 @@ export const equiAttributeArmor = {
           des: '哈皮毛毛',
           iconSrc: './icons/A_A9.png',
           entry: [{
-            'valCoefficient': 0.8,
-            'value': '11',
-            'showVal': '+11',
+            valCoefficient: 0.8,
+            value: '11',
+            showVal: '+11',
             type: 'DEF',
-            'name': '防御力'
+            name: '防御力'
           }, {
             type: 'HP',
-            'valCoefficient': 0.8,
-            'value': '8',
-            'showVal': '8',
-            'name': '生命值'
+            valCoefficient: 0.8,
+            value: '8',
+            showVal: '8',
+            name: '生命值'
           }, {
-            'valCoefficient': 0.4,
-            'value': '11',
-            'showVal': '+11',
+            valCoefficient: 0.4,
+            value: '11',
+            showVal: '+11',
             type: 'ATK',
-            'name': '攻击力'
+            name: '攻击力'
           }]
         }
       ],
--- src/assets/config/equiAttributeWeapon.js
+++ src/assets/config/equiAttributeWeapon.js
@@ -38,149 +38,149 @@ export const equiAttributeWeapon = {
         des: '',
         iconSrc: './icons/U_Sword01.png',
         entry: [{
-          'valCoefficient': 3.8,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 3.8,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
           type: 'DEF',
-          'valCoefficient': 1.2,
-          'value': '8',
-          'showVal': '8',
-          'name': '防御力'
+          valCoefficient: 1.2,
+          value: '8',
+          showVal: '8',
+          name: '防御力'
         }]
       }, {
         name: '阿加雷斯血色巨剑',
         des: '',
         iconSrc: './icons/U_Sword03.png',
         entry: [{
-          'valCoefficient': 2.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 2.2,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
           type: 'CRIT',
-          'valCoefficient': 1.2,
-          'value': '8',
-          'showVal': '+8%',
-          'name': '暴击率'
+          valCoefficient: 1.2,
+          value: '8',
+          showVal: '+8%',
+          name: '暴击率'
         }, {
-          'valCoefficient': 1.6,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.6,
+          value: '11',
+          showVal: '+11',
           type: 'CRITDMG',
-          'name': '暴击伤害'
+          name: '暴击伤害'
         }]
       }, {
         name: '死亡之镰',
         des: '',
         iconSrc: './icons/U_Sword04.png',
         entry: [{
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
           type: 'CRIT',
-          'valCoefficient': 1.2,
-          'value': '8',
-          'showVal': '+8%',
-          'name': '暴击率'
+          valCoefficient: 1.2,
+          value: '8',
+          showVal: '+8%',
+          name: '暴击率'
         }, {
-          'valCoefficient': 2.6,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 2.6,
+          value: '11',
+          showVal: '+11',
           type: 'CRITDMG',
-          'name': '暴击伤害'
+          name: '暴击伤害'
         }]
       }, {
         name: '霜龙利刃',
         des: '',
         iconSrc: './icons/U_Sword05.png',
         entry: [{
-          'valCoefficient': 2.6,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 2.6,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
           type: 'CRIT',
-          'valCoefficient': 1.2,
-          'value': '8',
-          'showVal': '+8%',
-          'name': '暴击率'
+          valCoefficient: 1.2,
+          value: '8',
+          showVal: '+8%',
+          name: '暴击率'
         }, {
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'CRITDMG',
-          'name': '暴击伤害'
+          name: '暴击伤害'
         }]
       }, {
         name: '大冒险家之剑',
         des: '',
         iconSrc: './icons/U_Sword07.png',
         entry: [{
-          'valCoefficient': 2.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 2.2,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
           type: 'CRIT',
-          'valCoefficient': 1.6,
-          'value': '8',
-          'showVal': '+8%',
-          'name': '暴击率'
+          valCoefficient: 1.6,
+          value: '8',
+          showVal: '+8%',
+          name: '暴击率'
         }, {
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'CRITDMG',
-          'name': '暴击伤害'
+          name: '暴击伤害'
         }]
       }, {
         name: '六翼天使之剑',
         des: '',
         iconSrc: './icons/U_Sword08.png',
         entry: [{
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
           type: 'CRIT',
-          'valCoefficient': 2.6,
-          'value': '8',
-          'showVal': '+8%',
-          'name': '暴击率'
+          valCoefficient: 2.6,
+          value: '8',
+          showVal: '+8%',
+          name: '暴击率'
         }, {
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'CRITDMG',
-          'name': '暴击伤害'
+          name: '暴击伤害'
         }]
       }, {
         name: '埃苏莱布斯之刀',
         des: '',
         iconSrc: './icons/U_Sword10.png',
         entry: [{
-          'valCoefficient': 3.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 3.2,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
           type: 'DEF',
-          'valCoefficient': 1.8,
-          'value': '8',
-          'showVal': '8',
-          'name': '防御力'
+          valCoefficient: 1.8,
+          value: '8',
+          showVal: '8',
+          name: '防御力'
         }]
       },],
       category: [{
@@ -188,17 +188,17 @@ export const equiAttributeWeapon = {
         des: '用狱岩石制作的太刀，据说拥有让使用者潜力爆发的神秘力量',
         iconSrc: './icons/W_Sword016.png',
         entry: [{
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
           type: 'CRIT',
-          'valCoefficient': 1.3,
-          'value': '8',
-          'showVal': '+8%',
-          'name': '暴击率'
+          valCoefficient: 1.3,
+          value: '8',
+          showVal: '+8%',
+          name: '暴击率'
         }]
       },
       {
@@ -206,17 +206,17 @@ export const equiAttributeWeapon = {
         des: '六级战士使用的长剑',
         iconSrc: './icons/W_Sword007.png',
         entry: [{
-          'valCoefficient': 1.2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.2,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
           type: 'DEF',
-          'valCoefficient': 0.5,
-          'value': '8',
-          'showVal': '8',
-          'name': '防御力'
+          valCoefficient: 0.5,
+          value: '8',
+          showVal: '8',
+          name: '防御力'
         }]
       },
       {
@@ -224,17 +224,17 @@ export const equiAttributeWeapon = {
         des: '似乎会给使用者提供生命气息',
         iconSrc: './icons/W_Sword019.png',
         entry: [{
-          'valCoefficient': 1.3,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.3,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
           type: 'HP',
-          'valCoefficient': 1.1,
-          'value': '8',
-          'showVal': '8',
-          'name': '生命值'
+          valCoefficient: 1.1,
+          value: '8',
+          showVal: '8',
+          name: '生命值'
         }]
       },
       {
@@ -242,11 +242,11 @@ export const equiAttributeWeapon = {
         des: '朴实无华普通长剑，有的只有强力的攻击力',
         iconSrc: './icons/W_Sword001.png',
         entry: [{
-          'valCoefficient': 1.7,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.7,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }]
       }
         ,
@@ -255,11 +255,11 @@ export const equiAttributeWeapon = {
         des: '传说中的狂战士最喜爱的剑。',
         iconSrc: './icons/W_Sword021.png',
         entry: [{
-          'valCoefficient': 1.7,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.7,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }]
       },
       {
@@ -267,17 +267,17 @@ export const equiAttributeWeapon = {
         des: '这？这也是武器？',
         iconSrc: './icons/W_Fist003.png',
         entry: [{
-          'valCoefficient': 2,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 2,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
           type: 'CRIT',
-          'valCoefficient': 0.7,
-          'value': '8',
-          'showVal': '+8%',
-          'name': '暴击率'
+          valCoefficient: 0.7,
+          value: '8',
+          showVal: '+8%',
+          name: '暴击率'
         }]
       },
       {
@@ -285,60 +285,60 @@ export const equiAttributeWeapon = {
         des: '剑锋覆盖着冰晶，碰到的敌人都会被冻住。',
         iconSrc: './icons/W_Sword018.png',
         entry: [{
-          'valCoefficient': 1.4,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.4,
+          value: '11',
+          showVal: '+11',
           type: 'ATK',
-          'name': '攻击力'
+          name: '攻击力'
         }, {
-          'valCoefficient': 1.3,
-          'value': '11',
-          'showVal': '+11',
+          valCoefficient: 1.3,
+          value: '11',
+          showVal: '+11',
           type: 'CRITDMG',
-          'name': '暴击伤害'
+          name: '暴击伤害'
         }]
       }
       ],
       extraEntry: [{
-        'value': '11',
-        'showVal': '+11',
+        value: '11',
+        showVal: '+11',
         type: 'ATK',
-        'name': '攻击力'
+        name: '攻击力'
       }, {
         type: 'CRIT',
-        'value': '8',
-        'showVal': '+8%',
-        'name': '暴击率'
+        value: '8',
+        showVal: '+8%',
+        name: '暴击率'
       }, {
         type: 'CRITDMG',
-        'value': '20',
-        'showVal': '+20%',
-        'name': '暴击伤害'
+        value: '20',
+        showVal: '+20%',
+        name: '暴击伤害'
       }, {
         type: 'HP',
-        'value': '20',
-        'showVal': '+20',
-        'name': '生命值'
+        value: '20',
+        showVal: '+20',
+        name: '生命值'
       }, {
         type: 'DEF',
-        'value': '8',
-        'showVal': '+8%',
-        'name': '防御力'
+        value: '8',
+        showVal: '+8%',
+        name: '防御力'
       }, {
-        'value': '11%',
-        'showVal': '+11%',
+        value: '11%',
+        showVal: '+11%',
         type: 'ATKPERCENT',
-        'name': '攻击力'
+        name: '攻击力'
       }, {
-        'value': '11%',
-        'showVal': '+11%',
+        value: '11%',
+        showVal: '+11%',
         type: 'DEFPERCENT',
-        'name': '防御力'
+        name: '防御力'
       }, {
-        'value': '11%',
-        'showVal': '+11%',
+        value: '11%',
+        showVal: '+11%',
         type: 'HPPERCENT',
-        'name': '生命值'
+        name: '生命值'
       },]
     }
   },
