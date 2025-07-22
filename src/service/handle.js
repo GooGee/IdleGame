7673389ee1bebd6b89b3ee4c9d3a86ec68b34ecf
GooGee <mou-gj@live.com>
2025-07-22 08:48:18 +0800

    add makeMonster in handle.js

--- src/service/handle.js
+++ src/service/handle.js
@@ -68,98 +68,83 @@ function CalculateStrAttr(entry, lv) {
 }
 
 /**
- * 随机化生成副本
- * @param {number} lv  副本等级
- * @param {number} difficulty(1:普通 2:困难 3:极难) 副本难度
+ * 
+ * @param {number} level 
+ * @param {number} difficulty 
+ * @returns 
  */
-function createRandomDungeons(lv, difficulty) {
-    lv = lv || 1, difficulty = difficulty || 1;
-    // 此处决定困难与极难的副本难度系数
-    // 困难 1.1
-    // 极难 1.25
-    var df = difficulty == 1 ? 1 : difficulty == 2 ? 1.1 : 1.25
-    var dungeonsConfig = {
-        id: lv + '' + difficulty,
+function makeAttribute(level, difficulty) {
+    const random = Math.random()
+    const HP = parseInt(difficulty * level ** 2 * (random * 5 + 11))
+    const ATK = parseInt(difficulty * level ** 2 * (random * 1 + 2))
+    return { HP, ATK, }
+}
+
+/**
+ * 
+ * @param {number} level 
+ * @param {number} difficulty 
+ * @returns 
+ */
+function makeMonster(level, difficulty) {
+    const gold = parseInt(difficulty * level ** 1.2 * (Math.random() * 5 + 11))
+    const equip = [
+        0.1 * difficulty,
+        0.2 * difficulty,
+        0.1 * difficulty,
+        0 * difficulty,
+    ]
+    return {
+        name: 'monster',
+        type: 'monster',
+        eventType: 'battle',
+        attribute: makeAttribute(level, difficulty),
+        trophy: { gold, equip, }
+    }
+}
+
+/**
+ * 随机化生成副本
+ * @param {number} level  副本等级
+ * @param {number} difficulty(1:容易 2:困难 3:极难) 副本难度
+ */
+function createRandomDungeons(level = 1, difficulty = 1) {
+    const factor = difficulty == 1 ? 1 : difficulty == 2 ? 1.2 : 1.5
+    const dungeonsConfig = {
+        id: level + '' + difficulty,
         battleTime: 2000,
-        name: 'Lv' + lv + '_' + (difficulty == 1 ? '普通' : difficulty == 2 ? '困难' : '极难'),
+        name: 'Lv' + level + '_' + (difficulty == 1 ? '容易' : difficulty == 2 ? '困难' : '极难'),
         eventNum: '5',
-        lv: lv,
-        needDPS: parseInt(lv * lv ** 1.3 * 2 * difficulty),
+        lv: level,
+        needDPS: parseInt(difficulty * level ** 2 * 3),
         difficulty: difficulty,
-        difficultyName: difficulty == 1 ? '普通' : difficulty == 2 ? '困难' : '极难',
+        difficultyName: difficulty == 1 ? '容易' : difficulty == 2 ? '困难' : '极难',
         top: Math.random() * 70 + 15 + '%',
         left: Math.random() * 70 + 10 + '%',
-        eventType: [{
-            name: 'monster',
-            type: 'monster',
-            eventType: 'battle',
-            attribute: {
-                HP: parseInt(lv * lv ** 1.1 * (Math.random() * 5 + 16) * df),
-                ATK: parseInt(lv * lv ** 1.1 * (Math.random() * 1 + 2) * df),
+        eventType: [
+            makeMonster(level, factor),
+            makeMonster(level, factor),
+            makeMonster(level, factor),
+            makeMonster(level, factor),
+            {
+                name: 'boss',
+                type: 'boss',
+                eventType: 'battle',
+                attribute: {
+                    HP: parseInt(factor * level ** 2 * (Math.random() * 5 + 33)),
+                    ATK: parseInt(factor * level ** 2 * (Math.random() * 2 + 3)),
+                },
+                trophy: {
+                    gold: parseInt(factor * level ** 2 * (Math.random() * 11 + 33)),
+                    equip: [
+                        0.2 - 0.1 * factor,
+                        0.5 - 0.2 * factor,
+                        0.2 + 0.2 * factor,
+                        0.1 + 0.1 * factor,
+                    ],
+                }
             },
-            trophy: {
-                gold: parseInt(lv ** 1.16 * (Math.random() * 5 + 11) * df),
-                equip: [
-                    0.2 * df, 0.08 * df, 0.03 * df, 0 * df
-                ],
-            }
-        }, {
-            name: 'monster',
-            type: 'monster',
-            eventType: 'battle',
-            attribute: {
-                HP: parseInt(lv * lv ** 1.1 * (Math.random() * 5 + 16) * df),
-                ATK: parseInt(lv * lv ** 1.1 * (Math.random() * 1 + 2) * df),
-            },
-            trophy: {
-                gold: parseInt(lv ** 1.16 * (Math.random() * 5 + 11) * df),
-                equip: [
-                    0.2 * df, 0.08 * df, 0.03 * df, 0 * df
-                ],
-            }
-        }, {
-            name: 'monster',
-            type: 'monster',
-            eventType: 'battle',
-            attribute: {
-                HP: parseInt(lv * lv ** 1.1 * (Math.random() * 5 + 16) * df),
-                ATK: parseInt(lv * lv ** 1.1 * (Math.random() * 1 + 2) * df),
-            },
-            trophy: {
-                gold: parseInt(lv ** 1.16 * (Math.random() * 5 + 11) * df),
-                equip: [
-                    0.2 * df, 0.08 * df, 0.03 * df, 0 * df
-                ],
-            }
-        }, {
-            name: 'monster',
-            type: 'monster',
-            eventType: 'battle',
-            attribute: {
-                HP: parseInt(lv * lv ** 1.1 * (Math.random() * 5 + 16) * df),
-                ATK: parseInt(lv * lv ** 1.1 * (Math.random() * 1 + 2) * df),
-            },
-            trophy: {
-                gold: parseInt(lv ** 1.16 * (Math.random() * 5 + 11) * df),
-                equip: [
-                    0.2 * df, 0.08 * df, 0.03 * df, 0 * df
-                ],
-            }
-        }, {
-            name: 'boss',
-            type: 'boss',
-            eventType: 'battle',
-            attribute: {
-                HP: parseInt(lv * lv ** 1.1 * (Math.random() * 5 + 30) * df),
-                ATK: parseInt(lv * lv ** 1.1 * (Math.random() * 1 + 3) * df),
-            },
-            trophy: {
-                gold: parseInt(lv ** 1.16 * (Math.random() * 10 + 28) * df),
-                equip: [
-                    0.25 - 0.05 * df, 0.55 - 0.15 * df, 0.15 + 0.15 * df, 0.05 + 0.05 * df
-                ],
-            }
-        },]
+        ]
     }
     return dungeonsConfig
 }
